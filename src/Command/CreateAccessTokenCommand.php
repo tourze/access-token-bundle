f897e8b9646ba9c09261ea2e92afa60b9ab8b5b0
github-ci <you@example.com>
2025-05-10 08:14:23 +0000

    update

--- src/Command/CreateAccessTokenCommand.php
+++ src/Command/CreateAccessTokenCommand.php
@@ -3,6 +3,7 @@
 namespace AccessTokenBundle\Command;
 
 use AccessTokenBundle\Service\AccessTokenService;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -10,7 +11,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use SymfonyEasyAdminDemo\Repository\UserRepository;
 
 #[AsCommand(
     name: 'app:create-access-token',
@@ -19,9 +19,10 @@ use SymfonyEasyAdminDemo\Repository\UserRepository;
 class CreateAccessTokenCommand extends Command
 {
     public function __construct(
-        private readonly UserRepository $userRepository,
+        private readonly UserLoaderInterface $userLoader,
         private readonly AccessTokenService $accessTokenService,
-    ) {
+    )
+    {
         parent::__construct();
     }
 
@@ -30,20 +31,19 @@ class CreateAccessTokenCommand extends Command
         $this
             ->addArgument('username', InputArgument::REQUIRED, '用户名')
             ->addOption('expires', 't', InputOption::VALUE_OPTIONAL, '令牌有效期（秒）', 86400)
-            ->addOption('device', 'd', InputOption::VALUE_OPTIONAL, '设备信息', null)
-        ;
+            ->addOption('device', 'd', InputOption::VALUE_OPTIONAL, '设备信息', null);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $username = $input->getArgument('username');
-        $expiresIn = (int) $input->getOption('expires');
+        $expiresIn = (int)$input->getOption('expires');
         $deviceInfo = $input->getOption('device');
 
         // 查找用户
-        $user = $this->userRepository->findOneBy(['username' => $username]);
-        
+        $user = $this->userLoader->loadUserByIdentifier($username);
+
         if (!$user) {
             $io->error(sprintf('用户 "%s" 不存在', $username));
             return Command::FAILURE;
@@ -52,7 +52,7 @@ class CreateAccessTokenCommand extends Command
         // 使用服务创建访问令牌
         $accessToken = $this->accessTokenService->createToken($user, $expiresIn, $deviceInfo);
         $expireDate = $accessToken->getExpiresAt()->format('Y-m-d H:i:s');
-        
+
         $io->success([
             sprintf('为用户 "%s" 生成了新的访问令牌', $username),
             sprintf('令牌值: %s', $accessToken->getToken()),
